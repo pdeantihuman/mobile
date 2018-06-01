9d283d1e6317330926e872b561e4d847abb2f569
yzz <819291502@qq.com>
2018-06-01 15:24:56 +0800

    fixed api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 export default {
-    list:'http://helper.test/api/articles?onlyRoot=1',
-    head_for_new:'http://helper.test/api/articles_root',
-    for_node:'http://helper.test/api/articles/',
+    list:'/api/articles?onlyRoot=1',
+    head_for_new:'/api/articles_root',
+    for_node:'/api/articles/',
 }
